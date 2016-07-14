834e3b74caf0cee76ba9132de958350ed70003eb
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2016-07-14 16:44:43 +0200

    FizzBuzz - implementation

--- 1/run.rb
+++ 1/run.rb
@@ -1,11 +1,27 @@
-(1..100).each do |x|
-if x % 5 == 0 and x % 3 == 0
-puts "FizzBuzz"
-elsif x % 3 == 0 
-puts "Fizz"
-elsif x % 5 == 0
-puts "Buzz"
-else
-puts x
+#
+# (1..100).each do |x|
+#     if x % 5 == 0 && x % 3 == 0
+#         puts 'FizzBuzz'
+#     elsif x % 3 == 0
+#         puts 'Fizz'
+#     elsif x % 5 == 0
+#         puts 'Buzz'
+#     else
+#         puts x
+#     end
+# end
+
+line = '3 5 10'
+
+class FizzBuzz
+  def initialize(line)
+    @line = line
+  end
+
+  def splited_line
+    @line.split(' ')
+  end
 end
-end
+
+obj = FizzBuzz.new(line)
+p obj.splited_line
